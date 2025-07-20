b99f4c8e5353f62680d878a4ffce301c2e7318e9
sadoon <muhammadsadoonsohail786@gmail.com>
2025-07-20 21:00:30 +0500

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 const heroSectionVideoEl = document.getElementsByTagName("video")[0];
+let isVideo = true;
 function showAnimation() {
     setTimeout(() => {
         heroSectionVideoEl.style.display = "none";
     }, 7200);
 }
-window.onload =  showAnimation();
+function showAnimation2() {
+    if (isVideo) {
+        isVideo = false;
+        document.getElementById("vedio2").src = "./assets/video/clip5-reversed.mp4"
+        document.getElementById("vedio2").play()
+    } else {
+        isVideo = true;
+        document.getElementById("vedio2").src = "./assets/video/clip5.mp4"
+        document.getElementById("vedio2").play()
+    }
+}
+window.onload = showAnimation();
+window.onload = setInterval(() => showAnimation2(), 12000);
